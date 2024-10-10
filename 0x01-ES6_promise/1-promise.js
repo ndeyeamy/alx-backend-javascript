f8bdea560f43d6c7f9d3d3b5548f0efa5a8786e4
ndeyeamy <falamy2006@gmail.com>
2024-10-10 20:59:56 +0000

    A function that returns a Promise based on a boolean

---
+++ 0x01-ES6_promise/1-promise.js
@@ -0,0 +1,3 @@
+export default function getFullResponseFromAPI(success) {
+  return success ? Promise.resolve({ status: 200, body: "Success" }) : Promise.reject(new Error("The fake API is not working currently"));
+}
