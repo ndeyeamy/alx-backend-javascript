8fa18ece27c1b10ab324d6b584046cfe3dd9afe7
ndeyeamy <falamy2006@gmail.com>
2024-10-10 12:27:30 +0000

    toi

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -2,11 +2,12 @@ export default function taskBlock(trueOrFalse) {
   const task = false;
   const task2 = true;
 
+  /* eslint-disable no-unused-vars */
   if (trueOrFalse) {
     const task = true;
     const task2 = false;
-    // Use the inner variables if needed, or return an array that includes them.
   }
+  /* eslint-disable no-unused-vars */
 
   return [task, task2];
 }
